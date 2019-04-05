70de8a8036bf6f6be6720211ec0741463868e815
G01\nakajima-naoya <nakajima-naoya@jp.fujitsu.com>
2019-04-05 17:57:37 +0900

    first commit

---
+++ src/main/java/com/fujitsu/tdd/test/RPS.java
@@ -0,0 +1,10 @@
+package com.fujitsu.tdd.test;
+
+public enum RPS {
+    ROCK,
+    PAPER,
+    SCISSORS,
+    P1_WINS,
+    P2_WINS,
+    TIE
+}
---
+++ src/main/java/com/fujitsu/tdd/test/RockPaperScissors.java
@@ -0,0 +1,9 @@
+package com.fujitsu.tdd.test;
+
+import static com.fujitsu.tdd.test.RPS.*;
+
+public class RockPaperScissors {
+    public RPS result(RPS p1, RPS p2) {
+        return P2_WINS;
+    }
+}
